c87e27603e78bfa31dc3279bf57e18aff0fe0854
mansimakawana67-wq <mansimakawana67@gmail.com>
2026-08-19 15:33:47 +0530

    link

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-// const bookingForm = document.querySelector(".booking1");
-// if (bookingForm) {
-//     bookingForm.addEventListener("submit", function (e) {
-//         e.preventDefault();
+const bookingForm = document.querySelector(".booking1");
+if (bookingForm) {
+    bookingForm.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-//         let checkin = document.getElementById("checkin").value;
-//         let checkout = document.getElementById("checkout").value;
-//         let guests = document.getElementById("guests").value;
-//         let room = document.getElementById("rooms").value;
+        let checkin = document.getElementById("checkin").value;
+        let checkout = document.getElementById("checkout").value;
+        let guests = document.getElementById("guests").value;
+        let room = document.getElementById("rooms").value;
 
-//         if (!checkin || !checkout) {
-//             alert("lease select check-in and check-out dates.");
-//             return;
-//         }
+        if (!checkin || !checkout) {
+            alert("lease select check-in and check-out dates.");
+            return;
+        }
 
-//         let bookingWhatsAppMessage = `🏨 New Villa Booking Request\n\n📅 Check-in Date: ${checkin}
-//         📅 Check-out Date: ${checkout}
-//         👥 Guests: ${guests}
-//         🏡 Room: ${room}`;
+        // Database ma data save karva
+        let formData = new FormData();
 
-//         let bookingWhatsAppURL = `https://wa.me/919316009175?text=${encodeURIComponent(bookingWhatsAppMessage)}`;
+        formData.append("checkin", checkin);
+        formData.append("checkout", checkout);
+        formData.append("guests", guests);
+        formData.append("room", room);
+        formData.append("book", "1");
 
-//         window.open(bookingWhatsAppURL, "_blank");
-//     });
-// }
+        fetch("index.php", {
+            method: "POST",
+            body: formData
+        })
+
+            .then(Response => Response.text())
+            .then(data => {
+                let bookingWhatsAppMessage = `🏨 New Villa Booking Request
+                📅 Check-in Date: ${checkin}
+                📅 Check-out Date: ${checkout}
+                👥 Guests: ${guests}
+                🏡 Room: ${room}`;
+
+
+
+                let bookingWhatsAppURL = `https://wa.me/919316009175?text=${encodeURIComponent(bookingWhatsAppMessage)}`;
+
+                window.open(bookingWhatsAppURL, "_blank");
+            })
+            .catch(error => {
+                console.log(error);
+                alert("Booking failed!");
+            });
+    });
+}
